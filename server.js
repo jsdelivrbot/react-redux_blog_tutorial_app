874453d9248e81793d14848a966a133942abe237
daern1991 <daniel_eriksson_8@hotmail.com>
2017-11-05 21:15:28 +0100

    Update server to listen to correct port for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
 app.use(express.static(__dirname + '/'));
 //////////////////////
 app.get('*', (req, res) =>{
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 //////////////////////
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+console.log("server started");
